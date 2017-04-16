3677cabc7efcf70cdf6b40a8badb68dba427afe5
Cassiano Honorio <honoriocassiano@gmail.com>
2017-04-16 20:19:54 -0300

    Adição do regex para telefone

--- tokenizer.py
+++ tokenizer.py
@@ -2,11 +2,24 @@ import re
 
 def main():
 
-	text = "Hoje é dia 15 de novembro de 2017. Hoje é dia 15 de novembro."
+	text = "Hoje é dia 15 de novembro de 2017. Hoje é dia 15 de novembro. Meu telefone é (21) 90999 9999"
 
 	dataRE = r"\d{1,2}(\sde\s(\w+|\d{1,2})(\sde\s(\d{4}|\d{2}))?|\/\d{1,2}(\/(\d{4}|\d{2}))?)"
+	foneRE = r"\(?(\d{2})?\)?\ ?\d{4,5}[-\ ]?\d{4}"
 
-	print(re.sub(dataRE, "_DATA_", text))
+	regexDict = {}
+	# Date Regex
+	regexDict[r"\d{1,2}(\sde\s(\w+|\d{1,2})(\sde\s(\d{4}|\d{2}))?|\/\d{1,2}(\/(\d{4}|\d{2}))?)"] = "_DATA_"
+	# Fone Regex
+	regexDict[r"\(?(\d{2})?\)?\ ?\d{4,5}[-\ ]?\d{4}"] = "_TELEFONE_"
+
+	newText = text
+
+	for k in regexDict:
+		newText = re.sub(k, regexDict[k], newText)
+
+	print("Original text: \"%s\"" % (text,))
+	print("Tokenized text: \"%s\"" % (newText,))
 
 	pass
 
