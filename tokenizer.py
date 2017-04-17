1e1841b7595a33129e2b09f55249ebd7431b6479
Cassiano Honorio <honoriocassiano@gmail.com>
2017-04-16 21:35:03 -0300

    Adição do regex para números

--- tokenizer.py
+++ tokenizer.py
@@ -2,9 +2,9 @@ import re
 
 def main():
 
-	text = "Hoje é dia 15 de novembro de 2017. Hoje é dia 15 de novembro. Meu telefone é (21) 90999 9999"
+	text = "Hoje é dia 15 de novembro de 2017. Hoje é dia 15 de novembro. Meu telefone é (21) 90999 9999. Tenho 21 anos."
 
-	dataRE = r"\d{1,2}(\sde\s(\w+|\d{1,2})(\sde\s(\d{4}|\d{2}))?|\/\d{1,2}(\/(\d{4}|\d{2}))?)"
+	dataRE = r"\d{1,2}(\ de\s(\w+|\d{1,2})(\ de\ (\d{4}|\d{2}))?|\/\d{1,2}(\/(\d{4}|\d{2}))?)"
 	foneRE = r"\(?(\d{2})?\)?\ ?\d{4,5}[-\ ]?\d{4}"
 
 	regexDict = {}
@@ -12,6 +12,9 @@ def main():
 	regexDict[r"\d{1,2}(\sde\s(\w+|\d{1,2})(\sde\s(\d{4}|\d{2}))?|\/\d{1,2}(\/(\d{4}|\d{2}))?)"] = "_DATA_"
 	# Fone Regex
 	regexDict[r"\(?(\d{2})?\)?\ ?\d{4,5}[-\ ]?\d{4}"] = "_TELEFONE_"
+
+	# Number Regex
+	regexDict[r"\d+\w*"] = "_NUMERO_"
 
 	newText = text
 
